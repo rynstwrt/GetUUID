cef22fd646450d28d6726c365797b62d5d92e8ca
Ryan Stewart <ryanstewartalex@gmail.com>
2024-06-01 03:23:29 -0500

    Init

---
+++ src/main/java/art/ryanstew/getuuid/GetUUID.java
@@ -0,0 +1,17 @@
+package art.ryanstew.getuuid;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GetUUID extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
